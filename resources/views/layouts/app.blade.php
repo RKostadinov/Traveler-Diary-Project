e60f7053736a0159b8e07f0136c429a4b7f9c107
Radoslav Kostadinov <Radoslav.j.kostadinov@gmail.com>
2016-04-13 16:24:10 +0300

    #8 Adding assets and views for gallery in places

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,10 @@
     <link rel="stylesheet" href="/assets/css/dropzone.css">
     <link rel="stylesheet" href="/assets/css/custom.css">
 
+    <link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap-image-gallery.min.css">
+    <link rel="stylesheet" href="/assets/css/thumbnail-gallery.css">
+
     <style>
         body {
             font-family: 'Lato';
@@ -87,6 +91,39 @@
             </div>
         </div>
     </nav>
+    <div id="blueimp-gallery" class="blueimp-gallery">
+        <!-- The container for the modal slides -->
+        <div class="slides"></div>
+        <!-- Controls for the borderless lightbox -->
+        <h3 class="title"></h3>
+        <a class="prev">‹</a>
+        <a class="next">›</a>
+        <a class="close">×</a>
+        <a class="play-pause"></a>
+        <ol class="indicator"></ol>
+        <!-- The modal dialog, which will be used to wrap the lightbox content -->
+        <div class="modal fade">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title"></h4>
+                    </div>
+                    <div class="modal-body next"></div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default pull-left prev">
+                            <i class="glyphicon glyphicon-chevron-left"></i>
+                            Previous
+                        </button>
+                        <button type="button" class="btn btn-primary next">
+                            Next
+                            <i class="glyphicon glyphicon-chevron-right"></i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     @yield('content')
 
@@ -95,6 +132,8 @@
     <script src="/assets/js/bootstrap.min.js"></script>
     <script src="/assets/js/jquery-ui.js"></script>
     <script src="/assets/js/dropzone.js"></script>
+    <script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+    <script src="/assets/js/bootstrap-image-gallery.min.js"></script>
 
     @yield('scripts')
 </body>
